e1fdb7876fe8e010441fd44e80f30276b3141388
Victor Chen <victorchen@Victors-MBP.wireless.newschool.edu>
2015-12-18 09:40:51 -0500

    updated js

--- timepiece/sketch.js
+++ timepiece/sketch.js
@@ -1,9 +1,34 @@
-var yoff = 0.0;        // 2nd dimension of perlin noise
+var yoff = 0.0;
+var wind;
+var position;       // 2nd dimension of perlin noise
+//var mySound;
+
 
 function setup() {
-  createCanvas(1700, 800);
-}
+  createCanvas(1700, 770);
 
+//function preload() {
+  //  mySound = loadSound('assets/beat.mp3');
+  //}
+//  mySound.setVolume(0.9);
+  //mySound.play();
+// amplitude = new p5.Amplitude(0.875);
+
+  //cnv.mouseClicked(function() {
+    //if (sound.isPlaying() ){
+      //sound.stop();
+    //} else {
+      //sound.play();
+    //}
+  //});
+  var url = 'http://api.openweathermap.org/data/2.5/weather?q=New%20York,NY&units=imperial&APPID=7bbbb47522848e8b9c26ba35c226c734';
+  loadJSON(url, gotWeather);
+  // Circle starts in the middle
+  position = createVector(width/2, height/2);
+  // wind starts as (0,0)
+  wind = createVector();
+
+}
 function draw() {
   background('orange');
 
@@ -19,15 +44,17 @@ function draw() {
     // Calculate a y value according to noise, map to
 
     // Option #1: 2D Noise
-    var y = map(noise(xoff, yoff), 0, 1, 100,600);
+    var y = map(noise(xoff, yoff), 0, 1, 100, 600);
 
     // Option #2: 1D Noise
     // float y = map(noise(xoff), 0, 1, 200,300);
 //var currentHour = hour();
-var currentMinute = 10;
-xoff += map(currentMinute, 1440, 1, 0.2, 0.002);
- yoff += map(currentMinute, 1440, 1, 0.007, 0.0001);
- console.log(xoff, yoff)
+var currentMinute = minute();
+xoff += map(currentMinute, 1440, 1, 0.2, 0.01);
+ yoff += map(currentMinute, 1440, 1, 0.01, 0.0003);
+ //console.log(xoff, yoff);
+ //background += map(currentMinute, 0, 128, 255);
+ //backround += map(currentMinute, 0, 10, 100);
 
   //  if(currentM > 8) {
   //   	 	xoff += 0.01;
@@ -46,13 +73,64 @@ xoff += map(currentMinute, 1440, 1, 0.2, 0.002);
     // Set the vertex
     vertex(x, y);
     // Increment x dimension for noise
-    xoff += 0.02;
+    xoff += 0.01;
   }
   // increment y dimension for noise
   yoff += -0.01;
   vertex(width, height);
   vertex(0, height);
   endShape(CLOSE);
+
+  push();
+  translate(32, height - 32);
+  // Rotate by the wind's angle
+  rotate(wind.heading() + PI/2);
+  noStroke();
+  fill(255);
+  ellipse(0, 0, 48, 48);
+
+  stroke(45, 123, 182);
+  strokeWeight(3);
+  line(0, -16, 0, 16);
+
+  noStroke();
+  fill(45, 123, 182);
+  triangle(0, -18, -6, -10, 6, -10);
+  pop();
+
+  // Move in the wind's direction
+  position.add(wind);
+
+  stroke(0);
+  fill(51);
+  ellipse(position.x, position.y, 16, 16);
+
+  if (position.x > width)  position.x = 0;
+  if (position.x < 0)      position.x = width;
+  if (position.y > height) position.y = 0;
+  if (position.y < 0)      position.y = height;
+
+
+}
+
+
+
+
+
+
+function gotWeather(weather) {
+console.log(weather);
+  // Get the angle (convert to radians)
+  var angle = radians(Number(weather.wind.deg));
+  // Get the wind speed
+  var windmag = Number(weather.wind.speed);
+
+  // Display as HTML elements
+  //var temperatureDiv = createDiv(floor(weather.main.temp) + '&deg;');
+  //var windDiv = createDiv("WIND " + windmag + " <small>MPH</small>");
+
+  // Make a vector
+  wind = p5.Vector.fromAngle(angle);
 }
 
 function windowResize() {
